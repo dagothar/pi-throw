4ac8441ed0be0cf5562ad3bd8e482271fc2856ca
Adam Wolniakowski <dagothar@gmail.com>
2017-03-14 22:53:46 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,10 @@ $(document).ready(function() {
   /* Draw board */
   function drawBoard() {
     context.beginPath();
+    context.strokeStyle = '#ffffff';
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fill();
+    context.beginPath();
     context.strokeStyle = '#00ff00';
     context.arc(canvas.width/2, canvas.height/2, radius, 0, 2 * Math.PI);
     context.stroke();
